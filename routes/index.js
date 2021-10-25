e416878f7d35e145d9e7e6680b805c4c3af08f5e
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-25 14:43:59 +0200

    Co-authored-by: Oscar <ogus02@users.noreply.github.com> Co-authored-by: apousette <apousette@users.noreply.github.com> changed file names and routes, ready to go

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => res.render('login'));
+
+router.get('/bookings', (req, res) => res.render('bookings'));
+
+
+router.post('/', (req, res) => {
+
 });
 
 module.exports = router;
