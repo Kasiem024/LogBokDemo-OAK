bad0f3300548b2915cdee945907b97de407f00de
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-26 10:18:38 +0200

    added function to show and hide bookinds

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,9 +19,8 @@ fetch('http://localhost:3000/bookingsList')
         return response.json();
     })
     .then(resBookings => {
-        let bookingsList = resBookings;
+        bookingsList = resBookings;
         console.log(bookingsList)
-        ShowBookings(bookingsList);
     });
 
 const Login = () => {
@@ -40,45 +39,52 @@ const Login = () => {
     });
 }
 
-const ShowBookings = (dataCalendar) => {
-    console.log('hej hej')
+const btnShowBookings = () => {
+    // const tBoxData = document.getElementById('tBoxBookTimeId')
+    // tBoxData.style.display = 'none';
 
-    const tBoxData = document.getElementById('tBoxBookTimeId')
-    tBoxData.style.display = 'none';
+    // const btnConfirm = document.createElement('button');
+    // btnConfirm.id = 'btnConfirmId';
+    // btnConfirm.textContent = 'Confirm';
+    // btnConfirm.disabled = true;
+    // document.getElementById('formId').appendChild(btnConfirm);
 
-    const btnConfirm = document.createElement('button');
-    btnConfirm.id = 'btnConfirmId';
-    btnConfirm.textContent = 'Confirm';
-    btnConfirm.disabled = true;
-    document.getElementById('formId').appendChild(btnConfirm);
+    const dataCalendar = bookingsList;
 
     for (let i = 0; i < dataCalendar.week.length; i++) {
 
         let mybr = document.createElement('br');
-        document.body.appendChild(mybr);
+        document.getElementById('calendar').appendChild(mybr);
 
         const p = document.createElement('p');
         p.textContent = dataCalendar.week[i].day
-        document.body.appendChild(p);
+        document.getElementById('calendar').appendChild(p);
 
         for (let j = 0; j < dataCalendar.week[i].times.length; j++) {
 
             const btn = document.createElement('button');
-            btn.textContent = "Boka " + dataCalendar.week[i].times[j].time;
+            btn.textContent = "Book " + dataCalendar.week[i].times[j].time;
             btn.style.width = "80px"
             btn.style.height = "80px"
             btn.id = i + "," + j;
             btn.className = 'btnBookClass';
 
-            document.body.appendChild(btn);
+            document.getElementById('calendar').appendChild(btn);
 
             if (dataCalendar.week[i].times[j].booked == true) {
                 btn.disabled = true;
             }
         }
     }
+    document.getElementById('btnShowBookingsId').disabled = true;
+    document.getElementById('btnHideBookingsId').disabled = false;
 };
 
+const btnHideBookings = () => {
+    document.getElementById('calendar').innerHTML = '';
+    document.getElementById('btnShowBookingsId').disabled = false;
+    document.getElementById('btnHideBookingsId').disabled = true;
+}
 const btnSignOut = () => {
-    location.href = '/login'
+    location.href = '/'
 }
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ const fs = require('fs');
 // TODO
 // Separera klienten och servern mer
 // Fler saker ska hända på server sidan
-// När en användare trycker på logga in skickas en GET och POST
+// När en användare vill logga in ska både GET och POST användas
 // Responsen blir en JA eller NEJ
 // Kan ha en bool i users.json, TRUE vid log in FALSE vid log ut
 // Detta leder till mer säkerhet
+// Två route filer
 
 
 /* GET home page. */
-router.get('/login', (req, res) => res.render('login'));
+router.get('/', (req, res) => res.render('login'));
 
 router.get('/bookings', (req, res) => res.render('bookings'));
 
